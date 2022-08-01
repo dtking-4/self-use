4ccda4310ecddee6149d338f65b5dd000cee7a76
dtking-4 <83275548+dtking-4@users.noreply.github.com>
2022-08-02 01:46:15 +0800

    Create b.sh

---
+++ b.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/dtking-4/self-use/main/surge.list -O /usr/share/caddy/surge_rules/SelfRule.list
+wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Lan/Lan.list -O /usr/share/caddy/surge_rules/Lan.list
+wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Clubhouse/Clubhouse.list -O /usr/share/caddy/surge_rules/Clubhouse.list
+wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Speedtest/Speedtest.list -O /usr/share/caddy/surge_rules/Speedtest.list
+wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/ChinaIPs/ChinaIPs.list -O /usr/share/caddy/surge_rules/ChinaIPs.list
+wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/China/China.list -O /usr/share/caddy/surge_rules/China.list
+wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/YouTube/YouTube.list -O /usr/share/caddy/surge_rules/YouTube.list
+wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Proxy/Proxy.list -O /usr/share/caddy/surge_rules/Proxy.list
+wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Proxy/Proxy_Domain.list -O /usr/share/caddy/surge_rules/Proxy_Domain.list
+wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Global/Global.list -O /usr/share/caddy/surge_rules/Global.list
+wget https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Global/Global_Domain.list -O /usr/share/caddy/surge_rules/Global_Domain.list
+
